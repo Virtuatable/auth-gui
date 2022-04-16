2286547ee338a87734cca85e623e034a098a92e8
Vincent Courtois <vincent.courtois@coddity.com>
2022-04-16 15:00:24 +0200

    Change the configuration to have static file names

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,16 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  configureWebpack: {
+    output: {
+      filename: "[name].js",
+      chunkFilename: "[name].chunk.js"
+    },
+  },
+  css: {
+    extract: {
+      filename: "[name].css",
+      chunkFilename: "[name].chunk.css"
+    }
+  }
 })
